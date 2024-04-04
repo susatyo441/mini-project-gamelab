975faa9036b8d0238f7654d601bd65273980bd70
Kyroline <rizky.w.dewantoro@gmail.com>
2024-04-04 13:26:25 +0700

    Landing page responsiveness improved

--- front-end/guest-site/src/components/Navbar.jsx
+++ front-end/guest-site/src/components/Navbar.jsx
@@ -23,11 +23,14 @@ const Navbar = () => {
     }, [])
 
     return (
-        <nav className={`fixed w-full h-20 top-0 left-0 flex flex-row z-10 px-3 transition-all duration-200 ${trans ? 'text-white' : 'text-black bg-white drop-shadow-md'}`}>
-            <div className="w-full items-center p-1 sm:p-2 sm:w-1/3 flex flex-row cursor-pointer">
+        <nav className={`z-50 fixed w-full h-20 top-0 left-0 flex flex-row z-10 transition-all duration-200 ${trans ? 'text-white' : 'text-black bg-white drop-shadow-md'}`}>
+            <div className="w-full items-center ml-3 p-1 sm:p-2 sm:w-1/3 flex flex-row cursor-pointer">
                 <div className="w-full">
                     <img className="mr-4 w-[100px] inline" src="./images/logo.png" />
                 </div>
+            </div>
+            <div className={`block sm:hidden h-full text-white ${trans ? '' : 'bg-red-600 hover:bg-red-800'} text-4xl py-5 px-4 cursor-pointer z-30`} onClick={{}}>
+                <i className='bi bi-filter-left rounded-md'></i>
             </div>
             <div className="hidden sm:w-1/3 sm:flex sm:flex-row font-[Poppins] font-medium">
                 <div className="w-1/3 h-full flex justify-center items-center">
